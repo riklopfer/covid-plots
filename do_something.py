f378ee72b946a945759651747e8c4258eadb2692
rklopfer <russell@klopfer.us>
2020-07-04 14:55:47 -0400

    compute moving averages

--- do_something.py
+++ do_something.py
@@ -17,7 +17,11 @@ DATA_DIR = "/tmp/covid-testing"
 
 
 def dl_csv_data(state):
-    out_dir = os.path.join(DATA_DIR, state)
+    """
+    https://covidtracking.com/api
+    """
+    # this doesn't have county-level testing data
+    out_dir = os.path.join(DATA_DIR, 'covidtracking', state)
     now_hour = datetime.utcnow().strftime('%Y-%m-%d:%H')
     out_path = os.path.join(out_dir, f'{now_hour}.daily.csv')
     # update once per hour?
@@ -35,8 +39,39 @@ def dl_csv_data(state):
     return out_path
 
 
-if __name__ == '__main__':
-    csv_path = dl_csv_data('pa')
+def set_test_rates(df, window):
+    totals = df[['totalTestResultsIncrease', 'positiveIncrease']].rolling(window).sum()
+    test_rates = totals['positiveIncrease'] / totals['totalTestResultsIncrease']
+    df[f'{window}DayRollingTestRate'] = test_rates
+
+
+def load_test_df(state):
+    csv_path = dl_csv_data(state)
+    """
+    ['date', 'state', 'positive', 'negative', 'pending',
+       'hospitalizedCurrently', 'hospitalizedCumulative', 'inIcuCurrently',
+       'inIcuCumulative', 'onVentilatorCurrently', 'onVentilatorCumulative',
+       'recovered', 'dataQualityGrade', 'lastUpdateEt', 'dateModified',
+       'checkTimeEt', 'death', 'hospitalized', 'dateChecked',
+       'totalTestsViral', 'positiveTestsViral', 'negativeTestsViral',
+       'positiveCasesViral', 'fips', 'positiveIncrease', 'negativeIncrease',
+       'total', 'totalTestResults', 'totalTestResultsIncrease', 'posNeg',
+       'deathIncrease', 'hospitalizedIncrease', 'hash', 'commercialScore',
+       'negativeRegularScore', 'negativeScore', 'positiveScore', 'score',
+       'grade']
+    """
     df = pd.read_csv(csv_path)
-    print(df)
+    # oldest to newest
+    df = df.reindex(index=df.index[::-1])
+    # index by date.
+    df.set_index('date', inplace=True)
+    # add rolling averages
+    for window in (3, 7, 14):
+        set_test_rates(df, window)
+    return df
+
+
+if __name__ == '__main__':
+    df = load_test_df('pa')
+    stuff = df[['totalTestResultsIncrease', 'positiveIncrease', '2DayRollingTestRate', '7DayRollingTestRate']]
     pass
